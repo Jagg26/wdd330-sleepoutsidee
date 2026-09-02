083d309c9b348fffae1f837ffa2127154cd886cc
jagg26 <john.ale26@gmail.com>
2026-09-02 20:17:30 -0300

    fix(cart): fix deploy issue

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,20 +1,19 @@
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
+import backpacks from "../json/backpacks.json";
+import sleepingBags from "../json/sleeping-bags.json";
+import tents from "../json/tents.json";
+
+const productData = {
+  backpacks,
+  "sleeping-bags": sleepingBags,
+  tents,
+};
 
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
   }
   getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+    return Promise.resolve(productData[this.category]);
   }
   async findProductById(id) {
     const products = await this.getData();
